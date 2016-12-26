0a4792a8a352f5f0c45492b0b6e4061bf3e4f97a
panzhibin <panzhibin@oa.gf.com.cn>
2016-12-26 15:40:01 +0800

    init project of broker

--- broker/run.sh
+++ broker/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo docker build -t console:latest .
+sudo docker build -t broker:latest .
 
 sudo docker images
 
-sudo docker run -v /var/log:/log --name console --restart=always -d -p 8080:8080 console:latest
+sudo docker run -v /var/log:/log --name broker --restart=always -d -p 8080:8080 broker:latest
 
 sudo docker ps

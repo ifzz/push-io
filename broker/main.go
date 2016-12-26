4cee8432ac6582d28114dfa13f520a398c5cdb6a
panzhibin <panzhibin@oa.gf.com.cn>
2016-12-26 16:19:19 +0800

    init project of broker

--- broker/main.go
+++ broker/main.go
@@ -14,15 +14,16 @@ func main() {
     iris.Get("/api/v1/server", func(ctx *iris.Context) {
 
         request := gorequest.New().SetBasicAuth(config.Username, config.Password)
-        request.SetDebug(true)
+        //request.SetDebug(true)
 
         for _, value := range config.Cluster {
             //fmt.Println(value)
-            resp, _, errs := request.Get(fmt.Sprintf("http://%s/api/stats", value)).End()
+            resp, body, errs := request.Get(fmt.Sprintf("http://%s/api/stats", value)).End()
             if (len(errs) > 0) {
                 continue
             }
-            fmt.Printf("%+v\n", resp)
+            fmt.Printf("resp: %+v\n", resp)
+            fmt.Printf("body: %+v\n", body);
         }
 
         ctx.JSON(iris.StatusOK, iris.Map{

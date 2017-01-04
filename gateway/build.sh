9ba62a511e2fd03d4f70a23c36e7784dd9e3dd32
panzhibin <panzhibin@oa.gf.com.cn>
2017-01-04 19:10:23 +0800

    add Dockerfile

--- gateway/build.sh
+++ gateway/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 sudo docker build -t gateway:latest .
+
+sudo docker images
--- gateway/run.sh
+++ gateway/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 sudo docker run --restart=always -itd --name gateway -p 3000:3000 console:latest
+
+sudo docker ps

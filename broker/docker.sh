64acceee34919628d9911d60c6a14564952a0635
panzhibin <panzhibin@oa.gf.com.cn>
2017-01-18 10:28:51 +0800

    update script

--- broker/docker.sh
+++ broker/docker.sh
@@ -4,6 +4,6 @@ sudo docker build -t broker:latest .
 
 sudo docker images
 
-sudo docker run -v /var/log:/log --name broker --restart=always -itd -p 8080:8080 broker:latest
+sudo docker run -v /var/log:/log --name broker --restart=always -itd -p 80:8080 broker:latest
 
 sudo docker ps
--- broker/upload.sh
+++ broker/upload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for host in '54.222.243.29'
+for host in '54.223.22.37'
 do
     echo $host
 

025b3bda5821bcb5877f64faba8b628b0c18a5e6
panzhibin <panzhibin@oa.gf.com.cn>
2017-01-19 15:15:21 +0800

    add product configuration

--- gateway/misc/config-product.js
+++ gateway/misc/config-product.js
@@ -1,3 +1,24 @@
 /**
  * Created by zhibinpan on 10/1/2017.
  */
+
+module.exports.LOG_DIR = null;
+
+// Default to 127.0.0.1
+module.exports.REDIS_HOST = '10.71.2.39';
+
+// Default to 6379
+module.exports.REDIS_PORT = 6379;
+
+module.exports.REDIS_OPTIONS = null;
+
+module.exports.ADMIN_CREDENTIALS = [
+    {
+        name: 'gftrader',
+        pass: '1163CFFD87155CD634CBD3DA9F53D'
+    },
+    {
+        name: 'gfnbop',
+        pass: '988733B9DDB81E626E4A84D232676'
+    }
+];

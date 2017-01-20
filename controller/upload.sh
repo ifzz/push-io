ff9da1c4d28c7d09bed58c10a60929f00415a070
panzhibin <panzhibin@oa.gf.com.cn>
2017-01-20 19:42:07 +0800

    add comments

--- controller/upload.sh
+++ controller/upload.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-for host in '54.223.146.80' '10.35.68.215'
+for host in '54.223.146.80'
 do
     echo $host
 
     scp ./main  ubuntu@$host:/home/ubuntu/push-io/controller/
+done
+
+for host in '10.35.68.215'
+do
+    echo $host
+
+    scp ./main  gf@$host:/home/gf/push-io/controller/
 done

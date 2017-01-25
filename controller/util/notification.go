32372eb5fef6c95ae2d1c50f4293231c01c3df66
panzhibin <panzhibin@oa.gf.com.cn>
2017-01-25 11:38:17 +0800

    find the message first before update

--- controller/util/notification.go
+++ controller/util/notification.go
@@ -20,15 +20,20 @@ type Notification struct {
 }
 
 func (n *Notification) PushAck() error {
+    s := session.Copy()
+    defer s.Close()
+    c := s.DB("dolphin").C("notification")
 
     now := time.Now()
+
+    result := Notification{}
+    err := c.Find(bson.M{"id": n.Id}).One(&result)
+    if err != nil {
+        return err
+    }
 
     query := bson.M{"id": n.Id}
     change := bson.M{"$set": bson.M{"ack": true, "timestamp": now.Unix(), "LastUpdated": now}}
 
-    s := session.Copy()
-    defer s.Close()
-
-    c := s.DB("dolphin").C("notification")
     return c.Update(query, change)
 }

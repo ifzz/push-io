d1cc1633419c26caf70a2afaa34863c77a806809
panzhibin <panzhibin@oa.gf.com.cn>
2017-02-14 16:42:35 +0800

    update single thread testing

--- broker/test/run.sh
+++ broker/test/run.sh
@@ -6,21 +6,22 @@ for i in {1..1024}
 do
     echo $i
 
-    curl --basic -u gftrader:1163CFFD87155CD634CBD3DA9F53D -k http://dolphin.gz.1251438792.clb.myqcloud.com/api/v1/server
+    #curl --basic -u gftrader:1163CFFD87155CD634CBD3DA9F53D -k http://dolphin.gz.1251438792.clb.myqcloud.com/api/v1/server
+
+    #printf "\n\n"
+
+    #curl http://dolphin.gz.1251438792.clb.myqcloud.com/api/v1/nodes
+
+    #printf "\n\n"
+
+    #curl -XPOST 'http://push-it.gf.com.cn/api/v1/notification'  -H 'Content-Type:application/json' --data '{"appId":"gftrader","appKey":"1163CFFD87155CD634CBD3DA9F53D","topic": "mike","message":{"payload":{"广发证券":"涨停", "招商证券":"跌停"}}}'
+    curl -XPOST 'http://54.223.22.37/api/v1/notification'  -H 'Content-Type:application/json' --data '{"appId":"demo","appKey":"demo","topic": "test","message":{"msg":{"top":"123"}, "type":"quoteStock"}}'
 
     printf "\n\n"
 
-    curl http://dolphin.gz.1251438792.clb.myqcloud.com/api/v1/nodes
+    #curl http://push-it.gf.com.cn/api/v1/message/1/10
 
-    printf "\n\n"
-
-    curl -XPOST 'http://push-it.gf.com.cn/api/v1/notification'  -H 'Content-Type:application/json' --data '{"appId":"gftrader","appKey":"1163CFFD87155CD634CBD3DA9F53D","topic": "mike","message":{"payload":{"广发证券":"涨停", "招商证券":"跌停"}}}'
-
-    printf "\n\n"
-
-    curl http://push-it.gf.com.cn/api/v1/message/1/10
-
-    printf "\n\n"
+    #printf "\n\n"
 
     #sleep 1
 done

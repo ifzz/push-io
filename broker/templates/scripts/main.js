3fc834db631961ffb70489d505911c85e6e95e3b
panzhibin <panzhibin@oa.gf.com.cn>
2017-02-17 10:43:33 +0800

    update UI prototype

--- broker/templates/scripts/main.js
+++ broker/templates/scripts/main.js
@@ -4,22 +4,29 @@
 
 $(function (){
 
+    var username;
+    var password;
+
     $('#signInBtn').click(function (event) {
 
-        var username = $('#inputUsername').val();
-        var password = $('#inputPassword').val();
+        username = $('#inputUsername').val();
+        password = $('#inputPassword').val();
 
         $.ajax({
             type: "POST",
             url: '/api/v1/login',
             data: JSON.stringify({
-                username: username,//'gftrader',
-                password: password,//'A98D8B1134D34F6E161463F757139'
+                username: username,
+                password: password,
             }),
             success: function (data, textStatus) {
                 console.log(textStatus);
                 $('#loginForm').hide();
                 $('#messageForm').show();
+            },
+            error: function (xhr, textStatus, error) {
+                "use strict";
+                alert(textStatus + ': ' + error);
             },
             contentType: 'application/json',
             dataType: 'json'
@@ -32,7 +39,25 @@ $(function (){
         var topic = $('#inputTopic').val();
         var text = $('#inputText').val();
 
-        try {
+        $.ajax({
+            type: "POST",
+            url: '/api/v1/notification',
+            data: JSON.stringify({
+                topic: topic,
+                message: {
+                    'content': text
+                },
+                appId: username,
+                appKey: password,
+            }),
+            success: function (data, textStatus) {
+                console.log(textStatus);
+            },
+            contentType: 'application/json',
+            dataType: 'json'
+        });
+
+        /*try {
             var obj = JSON.parse(text);
             if (typeof obj !== 'object') {
                 alert('must provide message in JSON format');
@@ -43,8 +68,8 @@ $(function (){
                     data: JSON.stringify({
                         topic: topic,
                         message: obj,
-                        appId: 'gftrader',
-                        appKey: 'A98D8B1134D34F6E161463F757139'
+                        appId: username,//'gftrader',
+                        appKey: password,//'A98D8B1134D34F6E161463F757139'
                     }),
                     success: function (data, textStatus) {
                         console.log(textStatus);
@@ -57,7 +82,7 @@ $(function (){
         catch (err) {
             console.error(err);
             alert('must provide message in JSON format');
-        }
+        }*/
 
         event.preventDefault();
     });
